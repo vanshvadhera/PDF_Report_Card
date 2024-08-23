e214c2895b8b46a2aa34b9ed34013c07195c6b6b
vansh vadhera <vanshvadhera@vanshs-MacBook-Pro-2.local>
2024-08-23 14:23:34 +0530

    teacherSign Added

--- src/App.js
+++ src/App.js
@@ -24,14 +24,29 @@ function App() {
       const imageUrl = userData.profileIMG;
       const imageBytes = await fetch(imageUrl).then((res) => res.arrayBuffer());
 
+      const teacherSignUrl =
+        "https://dpsin.s3.amazonaws.com/sign/IAteacher1.jpeg";
+      const teacherSignBytes = await fetch(teacherSignUrl).then((res) =>
+        res.arrayBuffer()
+      );
+
       // Embed the image in the PDF
       const image = await pdfDoc.embedJpg(imageBytes);
+      const teacherSignImage = await pdfDoc.embedJpg(teacherSignBytes);
+      
 
       // Get the first page
       const pages = pdfDoc.getPages();
       const firstPage = pages[0];
       const secondPage = pages[1];
       const thirdPage = pages[2];
+      const fourthPage = pages[3];
+      const fifthPage = pages[4];
+      const sixthPage = pages[5];
+      const seventhPage = pages[6];
+      const eighthPage = pages[7];
+      const ninthPage = pages[8];
+      const tenthPage = pages[9];
 
       // const { width, height } = image.scale(0.15); // Adjust scale as needed
       firstPage.drawImage(image, {
@@ -219,6 +234,436 @@ function App() {
         x: 348,
         y: 407,
         size: 10,
+        color: rgb(0, 0, 0),
+      });
+
+      fourthPage.drawText(userData.english.first.language, {
+        x: 295,
+        y: 695,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(userData.english.first.written, {
+        x: 295,
+        y: 680,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(
+        `${userData.english.first.description.slice(0, 45)}`,
+        {
+          x: 348,
+          y: 695,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      fourthPage.drawText(
+        `${userData.english.first.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 681,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      fourthPage.drawText(userData.english.second.word, {
+        x: 295,
+        y: 648,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(userData.english.second.Articular, {
+        x: 295,
+        y: 632,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(userData.english.second.proficiency, {
+        x: 295,
+        y: 617,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(
+        `${userData.english.second.description.slice(0, 45)}`,
+        {
+          x: 348,
+          y: 649,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      fourthPage.drawText(
+        `${userData.english.second.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 634,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      fourthPage.drawText(userData.hindi.first.language, {
+        x: 295,
+        y: 410,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(userData.hindi.first.written, {
+        x: 295,
+        y: 395,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(`${userData.hindi.first.description.slice(0, 45)}`, {
+        x: 348,
+        y: 410,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(`${userData.hindi.first.description.slice(45, 89)}`, {
+        x: 348,
+        y: 396,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(userData.hindi.second.word, {
+        x: 295,
+        y: 350,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(userData.hindi.second.Articular, {
+        x: 295,
+        y: 335,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(`${userData.hindi.second.description.slice(0, 45)}`, {
+        x: 348,
+        y: 350,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      fourthPage.drawText(
+        `${userData.hindi.second.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 335,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+
+      fifthPage.drawText("English", {
+        x: 210,
+        y: 630,
+        size: 18,
+        color: rgb(0, 0, 0),
+      });
+      fifthPage.drawText("Basketball", {
+        x: 115,
+        y: 145,
+        size: 18,
+        color: rgb(0, 0, 0),
+      });
+
+      sixthPage.drawText("English, Hindi, Maths", {
+        x: 80,
+        y: 620,
+        size: 15,
+        color: rgb(0, 0, 0),
+      });
+      sixthPage.drawText(
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        {
+          x: 80,
+          y: 490,
+          size: 15,
+          color: rgb(0, 0, 0),
+        }
+      );
+      sixthPage.drawImage(teacherSignImage, {
+        x: 80,
+        y: 100,
+        width: 100,
+        height: 50,
+      });
+
+      seventhPage.drawText(userData.english.first.language, {
+        x: 295,
+        y: 707,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(userData.english.first.written, {
+        x: 295,
+        y: 692,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(
+        `${userData.english.first.description.slice(0, 45)}`,
+        {
+          x: 348,
+          y: 707,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      seventhPage.drawText(
+        `${userData.english.first.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 693,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      seventhPage.drawText(userData.english.second.word, {
+        x: 295,
+        y: 662,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(userData.english.second.Articular, {
+        x: 295,
+        y: 647,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(userData.english.second.proficiency, {
+        x: 295,
+        y: 632,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(
+        `${userData.english.second.description.slice(0, 45)}`,
+        {
+          x: 348,
+          y: 662,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      seventhPage.drawText(
+        `${userData.english.second.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 648,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      seventhPage.drawText(userData.hindi.first.language, {
+        x: 295,
+        y: 465,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(userData.hindi.first.written, {
+        x: 295,
+        y: 450,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(`${userData.hindi.first.description.slice(0, 45)}`, {
+        x: 348,
+        y: 465,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(
+        `${userData.hindi.first.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 451,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      seventhPage.drawText(userData.hindi.second.word, {
+        x: 295,
+        y: 420,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(userData.hindi.second.Articular, {
+        x: 295,
+        y: 405,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      seventhPage.drawText(
+        `${userData.hindi.second.description.slice(0, 45)}`,
+        {
+          x: 348,
+          y: 420,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      seventhPage.drawText(
+        `${userData.hindi.second.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 407,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+
+      eighthPage.drawText(userData.english.first.language, {
+        x: 295,
+        y: 712,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(userData.english.first.written, {
+        x: 295,
+        y: 700,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(
+        `${userData.english.first.description.slice(0, 45)}`,
+        {
+          x: 348,
+          y: 712,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      eighthPage.drawText(
+        `${userData.english.first.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 701,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      eighthPage.drawText(userData.english.second.word, {
+        x: 295,
+        y: 668,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(userData.english.second.Articular, {
+        x: 295,
+        y: 652,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(userData.english.second.proficiency, {
+        x: 295,
+        y: 637,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(
+        `${userData.english.second.description.slice(0, 45)}`,
+        {
+          x: 348,
+          y: 669,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      eighthPage.drawText(
+        `${userData.english.second.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 654,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+      eighthPage.drawText(userData.hindi.first.language, {
+        x: 295,
+        y: 430,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(userData.hindi.first.written, {
+        x: 295,
+        y: 415,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(`${userData.hindi.first.description.slice(0, 45)}`, {
+        x: 348,
+        y: 430,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(`${userData.hindi.first.description.slice(45, 89)}`, {
+        x: 348,
+        y: 416,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(userData.hindi.second.word, {
+        x: 295,
+        y: 370,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(userData.hindi.second.Articular, {
+        x: 295,
+        y: 355,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(`${userData.hindi.second.description.slice(0, 45)}`, {
+        x: 348,
+        y: 370,
+        size: 10,
+        color: rgb(0, 0, 0),
+      });
+      eighthPage.drawText(
+        `${userData.hindi.second.description.slice(45, 89)}`,
+        {
+          x: 348,
+          y: 355,
+          size: 10,
+          color: rgb(0, 0, 0),
+        }
+      );
+
+      ninthPage.drawText("English, Hindi, Maths", {
+        x: 80,
+        y: 620,
+        size: 15,
+        color: rgb(0, 0, 0),
+      });
+      ninthPage.drawText(
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        {
+          x: 80,
+          y: 490,
+          size: 15,
+          color: rgb(0, 0, 0),
+        }
+      );
+
+      tenthPage.drawText("252", {
+        x: 460,
+        y: 655,
+        size: 18,
+        color: rgb(0, 0, 0),
+      });
+      tenthPage.drawText("152", {
+        x: 455,
+        y: 600,
+        size: 18,
+        color: rgb(0, 0, 0),
+      });
+      tenthPage.drawText("70%", {
+        x: 458,
+        y: 557,
+        size: 18,
         color: rgb(0, 0, 0),
       });
 
